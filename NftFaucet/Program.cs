3bc467e1d37aa8b661aa2c0a53ac34437def49d6
Ivan Yaremenchuk <trooper982@gmail.com>
2022-03-31 21:05:03 +0200

    Add MetaMask.Blazor

--- NftFaucet/Program.cs
+++ NftFaucet/Program.cs
@@ -1,3 +1,4 @@
+using MetaMask.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NftFaucet;
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddAntDesign();
+builder.Services.AddMetaMaskBlazor();
 
 await builder.Build().RunAsync();
