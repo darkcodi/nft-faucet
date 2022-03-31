23343d2f91d060da5add26a46ee2d0fa467316d4
Ivan Yaremenchuk <trooper982@gmail.com>
2022-03-31 20:50:27 +0200

    Add AntBlazor

--- NftFaucet/Program.cs
+++ NftFaucet/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddAntDesign();
 
 await builder.Build().RunAsync();
