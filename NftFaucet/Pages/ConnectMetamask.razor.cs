a51e0f58b858a5a634980785de3fcbebbb0c1edc
Ivan Yaremenchuk <trooper982@gmail.com>
2022-04-04 00:44:36 +0200

    Improve ConnectMetamask page

--- NftFaucet/Pages/ConnectMetamask.razor.cs
+++ NftFaucet/Pages/ConnectMetamask.razor.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using NftFaucet.Components;
 using NftFaucet.Extensions;
 
@@ -5,6 +7,13 @@ namespace NftFaucet.Pages;
 
 public class ConnectMetamaskComponent : BasicComponent
 {
+    [Inject]
+    protected IJSRuntime JsRuntime { get; set; }
+
+    protected bool HasMetaMask { get; set; }
+    protected string TitleText => $"You should {(HasMetaMask ? "connect" : "install")} MetaMask first";
+    protected string ButtonText => HasMetaMask ? "Connect" : "Install";
+
     protected override async Task OnInitializedAsync()
     {
         if (await Metamask.IsConnected())
@@ -12,10 +21,19 @@ public class ConnectMetamaskComponent : BasicComponent
             await Metamask.RefreshAddress();
             UriHelper.NavigateToRelative("/");
         }
+
+        HasMetaMask = Metamask.HasMetaMask ?? false;
     }
 
     protected async Task Connect()
     {
+        if (!HasMetaMask)
+        {
+            string url = "https://metamask.io/download/";
+            await JsRuntime.InvokeAsync<object>("open", url, "_blank");
+            return;
+        }
+
         var isConnected = await Metamask.Connect();
         if (isConnected)
         {
