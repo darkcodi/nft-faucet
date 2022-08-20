edd369e9fb7357bb35a83ad0a91cd213675f9e31
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-19 22:15:55 -0500

    Add real network names

--- NftFaucetRadzen/Models/NetworkModel.cs
+++ NftFaucetRadzen/Models/NetworkModel.cs
@@ -5,6 +5,7 @@ public class NetworkModel
     public string Name { get; set; }
     public ulong ChainId { get; set; }
     public string Currency { get; set; }
+    public string ImageName { get; set; }
     public bool IsSupported { get; set; }
     public bool IsTestnet { get; set; }
 }
