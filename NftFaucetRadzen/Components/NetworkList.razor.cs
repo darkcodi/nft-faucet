6dcf75dad85947826999dee399120eed20f5b128
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-20 10:14:13 -0500

    Add NetworkList component

---
+++ NftFaucetRadzen/Components/NetworkList.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+using NftFaucetRadzen.Models;
+
+namespace NftFaucetRadzen.Components;
+
+public partial class NetworkList
+{
+    [Parameter] public NetworkModel[] Data { get; set; }
+
+}
--- NftFaucetRadzen/Pages/Network.razor.cs
+++ NftFaucetRadzen/Pages/Network.razor.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using NftFaucetRadzen.Models;
 using Radzen;
-using Radzen.Blazor;
 
 namespace NftFaucetRadzen.Pages
 {
