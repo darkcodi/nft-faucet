8730457ae36bd9bd0ee8d844d40c4bfcf469f195
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-21 19:37:08 -0500

    Add ToggleSelection handler to NetworkList component

--- NftFaucetRadzen/Components/NetworkList.razor.cs
+++ NftFaucetRadzen/Components/NetworkList.razor.cs
@@ -7,4 +7,26 @@ public partial class NetworkList
 {
     [Parameter] public NetworkModel[] Data { get; set; }
     [Parameter] public Guid[] SelectedNetworkIds { get; set; }
+    [Parameter] public bool AllowMultipleSelection { get; set; }
+    [Parameter] public bool AllowUnselect { get; set; }
+
+    public void ToggleSelection(Guid networkId)
+    {
+        var selectedNetworks = SelectedNetworkIds?.ToList() ?? new List<Guid>();
+        var isAlreadySelected = selectedNetworks.Contains(networkId);
+        if (isAlreadySelected && AllowUnselect)
+        {
+            selectedNetworks.Remove(networkId);
+        }
+        else if (!isAlreadySelected)
+        {
+            if (!AllowMultipleSelection)
+            {
+                selectedNetworks.Clear();
+            }
+            selectedNetworks.Add(networkId);
+        }
+        SelectedNetworkIds = selectedNetworks.ToArray();
+        StateHasChanged();
+    }
 }
