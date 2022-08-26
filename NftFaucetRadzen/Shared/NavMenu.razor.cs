093bca020ea5628a0d4e67f2c20f6853910d4e32
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-26 18:10:18 -0500

    Move out logic from NavMenu.razor

--- NftFaucetRadzen/Shared/NavMenu.razor.cs
+++ NftFaucetRadzen/Shared/NavMenu.razor.cs
@@ -8,4 +8,13 @@ public partial class NavMenu : BasicComponent
 {
     protected Guid? SelectedNetworkId => AppState.Storage.SelectedNetworks?.FirstOrDefault();
     protected string SelectedNetworkName => Settings.Networks.FirstOrDefault(x => x.Id == SelectedNetworkId)?.ShortName;
+
+    private bool CollapseNavMenu { get; set; } = true;
+
+    private string NavMenuCssClass => CollapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        CollapseNavMenu = !CollapseNavMenu;
+    }
 }
