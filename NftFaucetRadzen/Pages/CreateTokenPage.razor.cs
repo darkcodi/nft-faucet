4853c3387a8f0e028e77aa278aa2a9af52d479d2
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-28 00:40:10 -0500

    Add CreateToken page

---
+++ NftFaucetRadzen/Pages/CreateTokenPage.razor.cs
@@ -0,0 +1,26 @@
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace NftFaucetRadzen.Pages;
+
+public partial class CreateTokenPage
+{
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    protected TooltipService TooltipService { get; set; }
+
+    [Inject]
+    protected ContextMenuService ContextMenuService { get; set; }
+
+    [Inject]
+    protected NotificationService NotificationService { get; set; }
+}
