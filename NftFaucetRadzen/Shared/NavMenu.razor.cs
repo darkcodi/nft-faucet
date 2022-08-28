8316f3cabafb0140894111a0524ef5e5884b49cc
Ivan Yaremenchuk <trooper982@gmail.com>
2022-08-27 23:54:40 -0500

    Remove non-needed properties from NavMenu

--- NftFaucetRadzen/Shared/NavMenu.razor.cs
+++ NftFaucetRadzen/Shared/NavMenu.razor.cs
@@ -6,14 +6,10 @@ namespace NftFaucetRadzen.Shared;
 
 public partial class NavMenu : BasicComponent
 {
-    protected Guid? SelectedNetworkId => AppState?.Storage?.SelectedNetworks?.FirstOrDefault();
-    protected string SelectedNetworkName => AppState?.Storage?.Networks?.FirstOrDefault(x => x?.Id == SelectedNetworkId)?.ShortName;
-    protected Guid? SelectedProviderId => AppState?.Storage?.SelectedProviders?.FirstOrDefault();
-    protected string SelectedProviderName => AppState?.Storage?.Providers?.FirstOrDefault(x => x?.Id == SelectedProviderId)?.ShortName;
-    protected Guid? SelectedContractId => AppState?.Storage?.SelectedContracts?.FirstOrDefault();
-    protected string SelectedContractName => AppState?.Storage?.Contracts?.FirstOrDefault(x => x?.Id == SelectedContractId)?.Symbol;
-    protected Guid? SelectedTokenId => AppState?.Storage?.SelectedTokens?.FirstOrDefault();
-    protected string SelectedTokenName => AppState?.Storage?.Tokens?.FirstOrDefault(x => x?.Id == SelectedTokenId)?.Name;
+    protected string SelectedNetworkName => AppState?.SelectedNetwork?.ShortName;
+    protected string SelectedProviderName => AppState?.SelectedProvider?.ShortName;
+    protected string SelectedContractName => AppState?.SelectedContract?.Symbol;
+    protected string SelectedTokenName => AppState?.SelectedToken?.Name;
 
     private bool CollapseNavMenu { get; set; } = true;
 
