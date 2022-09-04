4ded842c9ee008d8d1986e2ccb3a33d03ea8d827
Ivan Yaremenchuk <trooper982@gmail.com>
2022-09-04 11:44:01 -0500

    Add Icon support to CardList

--- NftFaucetRadzen/Components/CardList/CardListItemButton.cs
+++ NftFaucetRadzen/Components/CardList/CardListItemButton.cs
@@ -5,6 +5,7 @@ namespace NftFaucetRadzen.Components.CardList;
 public class CardListItemButton
 {
     public string Name { get; set; }
+    public string Icon { get; set; }
     public Action Action { get; set; }
     public ButtonStyle Style { get; set; }
 }
