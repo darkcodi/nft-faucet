82d1ecbc28035c15ebdf4e6090cde82e8c9ec2ab
Ivan Yaremenchuk <trooper982@gmail.com>
2022-09-24 23:12:24 -0500

    Add implementation of Mint function to SolanaKeygenProvider

--- NftFaucetRadzen/Plugins/ProviderPlugins/Keygen/Providers/SolanaKeygenProvider.cs
+++ NftFaucetRadzen/Plugins/ProviderPlugins/Keygen/Providers/SolanaKeygenProvider.cs
@@ -1,9 +1,16 @@
+using System.Text;
 using CSharpFunctionalExtensions;
 using NftFaucetRadzen.Components.CardList;
 using NftFaucetRadzen.Models;
 using NftFaucetRadzen.Plugins.NetworkPlugins;
 using NftFaucetRadzen.Utils;
+using Solana.Metaplex;
+using Solnet.Programs;
 using Solnet.Rpc;
+using Solnet.Rpc.Builders;
+using Solnet.Rpc.Models;
+using Solnet.Rpc.Utilities;
+using Solnet.Wallet;
 
 namespace NftFaucetRadzen.Plugins.ProviderPlugins.Keygen.Providers;
 
@@ -111,8 +118,115 @@ public class SolanaKeygenProvider : IProvider
     public Task<bool> EnsureNetworkMatches(INetwork network)
         => Task.FromResult(network.Type == NetworkType.Solana);
 
-    public Task<Result<string>> Mint(MintRequest mintRequest)
+    public async Task<Result<string>> Mint(MintRequest mintRequest)
     {
-        throw new NotImplementedException();
+        var client = ClientFactory.GetClient(mintRequest.Network.PublicRpcUrl.OriginalString);
+        var rentExemption = await client.GetMinimumBalanceForRentExemptionAsync(TokenProgram.MintAccountDataSize);
+
+        var wallet = new Wallet(Key.MnemonicPhrase);
+        var walletAddress = wallet.Account.PublicKey;
+        //var mintWallet = new Wallet(SolanaKey.GenerateNew().MnemonicPhrase);
+        var mint = wallet.GetAccount(1);
+        var mintAddress = mint.PublicKey;
+        var metadataAddress = GetMetadataAddress(mint.PublicKey);
+        var masterEditionAddress = GetMasterEditionAddress(mint.PublicKey);
+
+        // TOKEN METADATA
+        var data = new MetadataParameters()
+        {
+            name = mintRequest.Token.Name,
+            symbol = "SNFT",
+            uri = mintRequest.UploadLocation.Location,
+            creators = new List<Creator> { new Creator(wallet.Account.PublicKey, 100, true) },
+            sellerFeeBasisPoints = 924,
+        };
+
+        var destinationPublicKey = new PublicKey(mintRequest.DestinationAddress);
+
+        var instructions = new List<TransactionInstruction>();
+        
+        instructions.Add(SystemProgram.CreateAccount(walletAddress, mint, rentExemption.Result, TokenProgram.MintAccountDataSize, TokenProgram.ProgramIdKey));
+        instructions.Add(TokenProgram.InitializeMint(mint, 0, walletAddress, walletAddress));
+        instructions.Add(AssociatedTokenAccountProgram.CreateAssociatedTokenAccount(walletAddress, destinationPublicKey, mint));
+        var tokenBalanceAddress = AssociatedTokenAccountProgram.DeriveAssociatedTokenAccount(destinationPublicKey, mint);
+        instructions.Add(TokenProgram.MintTo(mint, tokenBalanceAddress, (ulong)mintRequest.TokensAmount, walletAddress));
+        
+        instructions.Add(MetadataProgram.CreateMetadataAccount(
+            metadataAddress,
+            mint,
+            walletAddress,
+            walletAddress,
+            walletAddress,
+            data,
+            true,
+            false
+        ));
+        instructions.Add(MetadataProgram.CreateMasterEdition(
+            1,
+            masterEditionAddress,
+            mint,
+            walletAddress,
+            walletAddress,
+            walletAddress,
+            metadataAddress
+        ));
+
+        var blockHash = (await client.GetRecentBlockHashAsync()).Result.Value.Blockhash;
+
+        var txBuilder = new TransactionBuilder()
+            .SetFeePayer(wallet.Account.PublicKey)
+            .SetRecentBlockHash(blockHash);
+
+        instructions.ForEach(x => txBuilder.AddInstruction(x));
+
+        var tx = txBuilder.Build(new List<Account> { wallet.Account, mint });
+        var simulationResult = await client.SimulateTransactionAsync(tx);
+        var isSimulationSuccessful = simulationResult.ErrorData == null;
+
+        if (!isSimulationSuccessful)
+        {
+            throw new Exception("Transaction simulation failed. Try again.");
+        }
+
+        var txResult = await client.SendTransactionAsync(tx);
+
+        return txResult.Result;
+    }
+
+    private PublicKey GetMetadataAddress(PublicKey mintAddress)
+    {
+        // PDA METADATA
+        AddressExtensions.TryFindProgramAddress(
+            new List<byte[]>() {
+                Encoding.UTF8.GetBytes("metadata"),
+                MetadataProgram.ProgramIdKey,
+                mintAddress
+            },
+            MetadataProgram.ProgramIdKey,
+            out var metadataAddressBytes,
+            out _
+        );
+        var metadataAddress = new PublicKey(metadataAddressBytes);
+
+        return metadataAddress;
+    }
+
+    private PublicKey GetMasterEditionAddress(PublicKey mintAddress)
+    {
+        // PDA MASTER EDITION
+        AddressExtensions.TryFindProgramAddress(
+            new List<byte[]>() {
+                Encoding.UTF8.GetBytes("metadata"),
+                MetadataProgram.ProgramIdKey,
+                mintAddress,
+                Encoding.UTF8.GetBytes("edition")
+            },
+            MetadataProgram.ProgramIdKey,
+            out var masterEditionAddressBytes,
+            out _
+        );
+        var masterEditionAddress = new PublicKey(masterEditionAddressBytes);
+
+        return masterEditionAddress;
     }
 }
