2663eda2cac7b7a6a4224b192c3f9e8c283baadc
Ivan Yaremenchuk <trooper982@gmail.com>
2022-10-02 22:42:08 -0500

    Add support for Sepolia network

--- plugins/network-plugins/NftFaucet.NetworkPlugins.Ethereum/Networks/SepoliaNetwork.cs
+++ plugins/network-plugins/NftFaucet.NetworkPlugins.Ethereum/Networks/SepoliaNetwork.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using NftFaucet.Domain.Models;
+using NftFaucet.Domain.Models.Abstraction;
 using NftFaucet.Domain.Models.Enums;
 using NftFaucet.Plugins.Models;
 
@@ -12,9 +15,34 @@ public sealed class SepoliaNetwork : Network
     public override int? Order { get; } = 7;
     public override string Currency { get; } = "SEP";
     public override string ImageName { get; } = "ethereum-gray.svg";
-    public override bool IsSupported { get; } = false;
     public override NetworkType Type { get; } = NetworkType.Ethereum;
     public override NetworkSubtype SubType { get; } = NetworkSubtype.Ethereum;
-    public override Uri PublicRpcUrl { get; } = null;
+    public override Uri PublicRpcUrl { get; } = new Uri("https://rpc.sepolia.org");
     public override Uri ExplorerUrl { get; } = new Uri("https://sepolia.etherscan.io/");
+
+    public override IReadOnlyCollection<IContract> DeployedContracts { get; } = new[]
+    {
+        new Contract
+        {
+            Id = Guid.Parse("c00521a1-af2e-44c3-8af2-d83426f79d62"),
+            Name = "ERC-721 Faucet",
+            Symbol = "FA721",
+            Address = "0x9F64932Be34D5D897C4253D17707b50921f372B6",
+            Type = ContractType.Erc721,
+            DeploymentTxHash = "0xfa17519ea84d95c346c6803d64ba71ab22455a0a9ffbe3803be6e7bd9cc8a371",
+            DeployedAt = DateTime.Parse("Oct-03-2022 03:33:36 AM", CultureInfo.InvariantCulture),
+            IsVerified = true,
+        },
+        new Contract
+        {
+            Id = Guid.Parse("bd3bb27b-827c-4936-a7ed-f7e8449ba97a"),
+            Name = "ERC-1155 Faucet",
+            Symbol = "FA1155",
+            Address = "0x1eD60FedfF775D500DDe21A974cd4E92e0047Cc8",
+            Type = ContractType.Erc1155,
+            DeploymentTxHash = "0x2991cffbd195514207b4d0880530690be351d2bdd03bb29d3442552225b0dd79",
+            DeployedAt = DateTime.Parse("Oct-03-2022 03:36:36 AM", CultureInfo.InvariantCulture),
+            IsVerified = true,
+        },
+    };
 }
